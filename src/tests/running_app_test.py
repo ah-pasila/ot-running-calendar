094a57626daa0816cc45c9cdc2ba28e64533751f
Pasila Aura H <ahpasila@melkki.cs.helsinki.fi>
2023-11-16 21:04:47 +0200

    Sovelluksen etstirakenteita lisätty

---
+++ src/tests/running_app_test.py
@@ -0,0 +1,9 @@
+import unittest
+from running_app import Running_app
+
+class Test Runningapp(unittest.TestCase):
+    def setUp(self):
+        print("Set up goes here")
+
+    def test(self):
+        print("Eka testi")
